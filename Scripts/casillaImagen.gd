ef754d667f3d8e92a709c3940b32625f946d4e14
pegasojuegos <mhp74185@gmail.com>
2024-08-13 18:01:07 +0200

    Implementado liberación de casillas
    He añadido un efecto al liberarse haciendo que cada casilla tarde 0,1 segundos en seguir liberando, queda bien pero no se si afecta al rendimiento demasiado, mirar si dejar o no

--- Scripts/casillaImagen.gd
+++ Scripts/casillaImagen.gd
@@ -1,16 +1,18 @@
 extends Sprite2D
 
-@export var desmarcada: bool
+
 
 signal meHicieronClick()
 
 func _input(event):
-	if !desmarcada and event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and get_rect().has_point(to_local(event.position)):
-		desmarcada = true
-		$".".texture=ResourceLoader.load("res://assets/desmarcada.png");
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and get_rect().has_point(to_local(event.position)):
 		meHicieronClick.emit()
 
 
-func _on_casilla_cambiar_estado(estado):
-	if estado == 'x':
-		$".".texture=ResourceLoader.load("res://assets/bomba.png");
+func _on_casilla_cambiar_estado(estado,n):
+	match estado:
+		'x': $".".texture=ResourceLoader.load("res://assets/bomba.png")
+		'n': 
+			var texture_path = "res://assets/" + str(n) + ".png"
+			$".".texture = ResourceLoader.load(texture_path)
+		'': $".".texture=ResourceLoader.load("res://assets/desmarcada.png")
--- Scripts/juego.gd
+++ Scripts/juego.gd
@@ -9,36 +9,81 @@ var casilla_scene: PackedScene = preload("res://escenas/casilla.tscn")
 
 func _ready():
 	casillas = Array()
-	for i:int in range(tamaño):
+	#Creamos un array y en cada casilla i metemos un array (Creación de matriz)
+	for i:int in range(tamaño): 
 		casillas.append(Array())
-		for j:int in range(tamaño): #i * tamaño + j
+		for j:int in range(tamaño): 
+			#Por cada casilla j de la matriz creamos una instancia de cailla
 			var casilla = casilla_scene.instantiate()
+			#Lo añadimos al arbol de Juego
 			$contenedorCasillas.add_child(casilla)
+			#Lo colocamos en la posición de pantalla deseada.
 			casilla.position = Vector2(i*separacionCasillas,j*separacionCasillas)
+			#Guardamos en el propio objeto su ubicación (i,j) que tendrá dentro de la matriz
 			casilla.pos = Vector2(i,j)
+			#Conectamos la señal buscar de casilla con un receptor aquí en juego
 			casilla.connect("buscar", Callable(self, "_on_casilla_buscar"))
+			#Guadamos la casilla creada en la matriz
 			casillas[i].append(casilla)
 	
+	#Creación de n bombas
 	for n:int in range(nBombas):
 		var hayBomba = true
 		var x:int
 		var y:int
-		while hayBomba:
+		#Imitación de doWhile, para que genere un número y lo vuelva a generar hasta que en esa posición no haya una bomba
+		while hayBomba: 
 			x = (randf_range(0,tamaño-1))
 			y = (randf_range(0,tamaño-1))
 			if casillas[x][y].contenido != 'x':
 				hayBomba = false
 		
+		#Añade una bomba a la posición 
 		casillas[x][y].contenido = 'x'
-		if x < tamaño-1: casillas[x+1][y].nBombasCerca += 1
-		if y < tamaño-1: casillas[x][y+1].nBombasCerca += 1
-		if x < tamaño-1 && y < tamaño-1: casillas[x+1][y+1].nBombasCerca += 1
-		if x > 0 && y > 0: casillas[x-1][y-1].nBombasCerca += 1
-		if x < tamaño-1 && y > 0: casillas[x+1][y-1].nBombasCerca += 1
-		if x > 0 && y < tamaño-1: casillas[x-1][y+1].nBombasCerca += 1
-		if y > 0: casillas[x][y-1].nBombasCerca += 1 
-		if x > 0: casillas[x-1][y].nBombasCerca += 1
+		# Para cada casilla de alrededor que este dentro del mapa y no tenga bomba
+		# suma uno a sus bombas cercanas y cambio su contenido
+		if x < tamaño-1 && casillas[x+1][y].contenido!= 'x': 
+			casillas[x+1][y].nBombasCerca += 1
+			casillas[x+1][y].contenido = 'n'
+		if y < tamaño-1 && casillas[x][y+1].contenido!= 'x':
+			casillas[x][y+1].nBombasCerca += 1
+			casillas[x][y+1].contenido = 'n'
+		if x < tamaño-1 && y < tamaño-1 && casillas[x+1][y+1].contenido!= 'x': 
+			casillas[x+1][y+1].nBombasCerca += 1
+			casillas[x+1][y+1].contenido = 'n'
+		if x > 0 && y > 0 && casillas[x-1][y-1].contenido!= 'x': 
+			casillas[x-1][y-1].nBombasCerca += 1
+			casillas[x-1][y-1].contenido = 'n'
+		if x < tamaño-1 && y > 0 && casillas[x+1][y-1].contenido!= 'x': 
+			casillas[x+1][y-1].nBombasCerca += 1
+			casillas[x+1][y-1].contenido = 'n'
+		if x > 0 && y < tamaño-1 && casillas[x-1][y+1].contenido!= 'x': 
+			casillas[x-1][y+1].nBombasCerca += 1
+			casillas[x-1][y+1].contenido = 'n'
+		if y > 0 && casillas[x][y-1].contenido!= 'x': 
+			casillas[x][y-1].nBombasCerca += 1 
+			casillas[x][y-1].contenido = 'n'
+		if x > 0 && casillas[x-1][y].contenido!= 'x': 
+			casillas[x-1][y].nBombasCerca += 1
+			casillas[x-1][y].contenido = 'n'
 
 
 func _on_casilla_buscar(pos: Vector2):
-	print(casillas[pos.x][pos.y].nBombasCerca)
+	var x = pos.x
+	var y = pos.y
+	if x < tamaño-1 && casillas[x+1][y].contenido != 'x' && !casillas[x+1][y].desmarcada: 
+		casillas[x+1][y].desmarcar()
+	if y < tamaño-1 && casillas[x][y+1].contenido != 'x' && !casillas[x][y+1].desmarcada:
+		casillas[x][y+1].desmarcar()
+	if x < tamaño-1 && y < tamaño-1 && casillas[x+1][y+1].contenido != 'x' && !casillas[x+1][y+1].desmarcada: 
+		casillas[x+1][y+1].desmarcar()
+	if x > 0 && y > 0 && casillas[x-1][y-1].contenido != 'x' && !casillas[x-1][y-1].desmarcada: 
+		casillas[x-1][y-1].desmarcar()
+	if x < tamaño-1 && y > 0 && casillas[x+1][y-1].contenido != 'x' && !casillas[x+1][y-1].desmarcada: 
+		casillas[x+1][y-1].desmarcar()
+	if x > 0 && y < tamaño-1 && casillas[x-1][y+1].contenido != 'x' && !casillas[x-1][y+1].desmarcada: 
+		casillas[x-1][y+1].desmarcar()
+	if y > 0 && casillas[x][y-1].contenido != 'x' && !casillas[x][y-1].desmarcada: 
+		casillas[x][y-1].desmarcar()
+	if x > 0 && casillas[x-1][y].contenido != 'x' && !casillas[x-1][y].desmarcada: 
+		casillas[x-1][y].desmarcar()
